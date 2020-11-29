5cb3aa860bd2eb114e9c2417017ad0896bd03422
Br4ndonP0nce <brandoncompany876@gmail.com>
2020-11-29 14:37:20 -0600

    updates load/save from json

---
+++ Assets/Scriptable Objects/CardDB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="cards database",menuName ="Inventory sys/Card Database")]
+public class CardDB : ScriptableObject, ISerializationCallbackReceiver
+{
+    public Card[] cards;
+    public Dictionary<Card, int> GetId = new Dictionary<Card, int>();
+    public Dictionary<int, Card> GetCard = new Dictionary<int, Card>();
+
+    public void OnAfterDeserialize()
+    {
+        GetId = new Dictionary<Card, int>();
+        GetCard = new Dictionary<int, Card>();
+        for (int i =0;i<cards.Length;i++)
+        {
+            GetId.Add(cards[i], i);
+            GetCard.Add(i, cards[i]);
+        }
+
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
+
+}
--- Assets/Scriptable Objects/Inventory/Scripts/InvObject.cs
+++ Assets/Scriptable Objects/Inventory/Scripts/InvObject.cs
@@ -2,45 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
-[CreateAssetMenu(fileName ="New Inventory",menuName ="Inventory sys/Inventory")]
-public class NewBehaviourScript : ScriptableObject
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+[CreateAssetMenu(fileName ="New Inventory",menuName ="Inventory sys/InventorySlots")]
+public class NewBehaviourScript : ScriptableObject,ISerializationCallbackReceiver
 {
+    JsonData toload;
     public List<Slots> SlotList = new List<Slots>();
-
+    public CardDB db;
+    public string path;
     public void AddCard(Card _card, int _ammount)
     {
-        bool exists = false;
         for (int i =0; i <SlotList.Count; i++)
         {
             if (SlotList[i].card == _card)
             {
                 SlotList[i].addAmm(_ammount);
-                exists = true;
-                break;
+                
+                return;
             }
         }
+        SlotList.Add(new Slots(db.GetId[_card],_card, _ammount));
 
-        if (!exists)
-        {
-            SlotList.Add(new Slots(_card, _ammount));
-        }
+
+
+
     }
 
-
+    public void saveJson()
+    {
+        string saveD = JsonUtility.ToJson(this,true);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(string.Concat(Application.persistentDataPath, path));
+        bf.Serialize(file, saveD);
+        file.Close();
+    }
     public void loadJson()
     {
-        //JsonData toload = JsonMapper();
+        if (File.Exists(string.Concat(Application.persistentDataPath,path)))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(string.Concat(Application.persistentDataPath,path),FileMode.Open);
+            JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+            file.Close();
+        }
+        
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
+    }
+
+    public void OnAfterDeserialize()
+    {
+        for(int i = 0; i < SlotList.Count; i++)
+        {
+            SlotList[i].card = db.GetCard[SlotList[i].id];
+        }
     }
 }
 
 [System.Serializable]
 public class Slots
 {
+    public int id;
     public Card card;
     public int ammount;
 
-    public Slots(Card _card,int _ammount)
+    public Slots(int _id,Card _card,int _ammount)
     {
+        id = _id;
         card = _card;
         ammount = _ammount;
     }
