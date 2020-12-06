4d2d671ffb7c964b85b257be41302dc64db00f7f
Br4ndonP0nce <brandoncompany876@gmail.com>
2020-12-05 22:17:21 -0600

    Improved inventory
    Changed from multi prefab to single prefab tree, added slotting function instead of prefab adding.

--- Assets/Scriptable Objects/Database/Scripts/CardDB.cs
+++ Assets/Scriptable Objects/Database/Scripts/CardDB.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "New Card Database", menuName = "Inventory/Card Database")]
-public class CardDB : ScriptableObject //ISerializationCallbackReceiver
+public class CardDB : ScriptableObject,ISerializationCallbackReceiver
 {
 
     public List<Card> allCards = new List<Card>();
-
-    public Dictionary<Card, int> GetId = new Dictionary<Card, int>();
     public Dictionary<int, Card> GetCard = new Dictionary<int, Card>();
+
+
+
+    public void OnAfterDeserialize()
+    {
+        
+        for (int i = 0; i < allCards.Count; i++)
+        {
+            allCards[i].ID = i;
+            GetCard.Add(i, allCards[i]);
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        GetCard = new Dictionary<int, Card>();
+    }
 }
